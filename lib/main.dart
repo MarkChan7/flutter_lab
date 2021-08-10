f2b2187f93a9066f5d0fb31fca7930b2b513d9be
Mark Chan <markchan163@163.com>
2021-08-10 14:26:09 +0800

    User an external packgage - english words

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   // runApp(MyApp());
@@ -8,6 +9,7 @@ void main() {
 class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,7 +17,7 @@ class HelloWorldApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
